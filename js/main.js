5d4ad5e008fec9df759422d792e0047452ef07e9
Prince-Attrish <vs3031999@gmail.com>
2023-08-17 09:09:04 +0530

    about & skills

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
     $('#ripples').ripples({
         resolution: 512,
         dropRadius: 10
+    });
+
+    const bars = document.querySelectorAll('.progress__bar');
+    
+    bars.forEach((bar) => {
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
     })
+
 });
