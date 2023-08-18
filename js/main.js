fa4df29ff49ecc26aea4df76c2aa6852d9d8b77f
Prince-Attrish <vs3031999@gmail.com>
2023-08-18 19:33:53 +0530

    completed structure

--- js/main.js
+++ js/main.js
@@ -14,4 +14,71 @@ $(document).ready(function() {
         bar.style.width = percentage + '%';
     })
 
+
+    // Image Fillter
+
+    var $wrapper = $('.portfolio__wrapper');
+
+    // Initialize Isotope for all
+    $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions : {
+            duration : 750,
+            easing : 'linear'
+        }
+    });
+
+    let links = document.querySelectorAll('.tabs a');
+
+    links.forEach(link => {
+
+        let selector = link.dataset.filter;
+
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            // Initialize Isotope for each tab
+            $wrapper.isotope({
+                filter : selector,
+                layoutMode : 'masonry',
+                animationOptions : {
+                    duration : 750,
+                    easing : 'linear'
+                }
+            });
+
+            
+            links.forEach(link => {
+                link.classList.remove('active');
+            })
+            
+            e.target.classList.add('active');
+
+        });
+    });
+
+    // Magnific Popup
+    $('.magnific_popup').magnificPopup({
+        type : 'image',
+        gallery : {
+            enabled : true
+        },
+        zoom : {
+            enable : true
+        }
+    });
+
+    // Slick Slider
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true
+    });
+
+    // Print resume
+    var printBtn = document.getElementById('print');
+
+    printBtn.addEventListener('click', () => {
+        print();
+    })
+
 });
